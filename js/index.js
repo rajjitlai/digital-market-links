407381c7cdf2f45a1e2568e8edb6ed38b3df6b7a
RAJJIT <2.0lairaj@gmail.com>
2024-06-19 09:49:16 +0530

    v-2.6-fixes

--- js/index.js
+++ js/index.js
@@ -2,16 +2,24 @@ let navbar = document.querySelector('nav')
 let menuLinks = document.getElementById("menuShow")
 let iconChange = document.getElementById("iconChange")
 
-window.onscroll = function() {
-    if(window.scrollY > 0){
+window.onscroll = function () {
+    if (window.scrollY > 0) {
         navbar.style.background = "#fff";
-    }else{
+    } else {
         navbar.style.background = "transparent";
     }
 }
 
 function toggleMenu() {
     menuLinks.classList.toggle("showMenu");
-    iconChange.classList.add("uil-times")
-    iconChange.classList.remove("uil-bars")
 }
+
+iconChange.addEventListener("click", function () {
+    if (iconChange.classList.contains('uil-bars')) {
+        iconChange.classList.remove('uil-bars');
+        iconChange.classList.add('uil-times');
+    } else {
+        iconChange.classList.remove('uil-times');
+        iconChange.classList.add('uil-bars');
+    }
+});
