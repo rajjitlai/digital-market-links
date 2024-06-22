2d23becc53e8e8978f21669fb68ee9b6b099e84f
RAJJIT <2.0lairaj@gmail.com>
2024-06-22 21:54:25 +0530

    v-5.6-contact-working

--- js/index.js
+++ js/index.js
@@ -31,3 +31,24 @@ AOS.init({
 });
 
 // contact and newsletter
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwDz4jyXJtUk-uzHIRO-hpjIAyOrnHERVyBLMbgNal35DSTtT1HMLM_bHf0u0XpfDK1LA/exec'
+const form = document.forms['submit-to-google-sheet']
+
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Thank you for contacting us! We may use your email address to contact you later! Kindly check your email and spam folders!"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 10000)
+
+            form.reset()
+        })
+        .catch(error => {
+            msg.innerHTML = "Your message could not be sent!"
+            msg.style.color = "red"
+        })
+})
