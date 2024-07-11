624d09da160a9012a264c9e7a2d0a2b679cc8d5b
RAJJIT <2.0lairaj@gmail.com>
2024-07-11 20:50:12 +0530

    v9.8-navbar

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ function showMenu() {
 function hideMenu() {
     document.getElementById('navLinks').classList.remove('show-menu');
 }
+
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('scrollNavbar');
+    if (window.scrollY > 100) {
+        navbar.style.display = 'block';
+    } else {
+        navbar.style.display = 'none';
+    }
+});
