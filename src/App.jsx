bcbc82bcfd8ff00e96cf0feae80fe33de28a5c34
RAJJIT <2.0lairaj@gmail.com>
2025-01-28 20:36:46 +0530

    new init

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import "./index.css";
-import Footer from "./components/Footer";
+import React from 'react'
 
 const App = () => {
   return (
-    <div className="bg-bgPrimary min-h-screen flex flex-col">
-      <Navbar />
-      <div className="flex-grow">
-        <Outlet />
-      </div>
-      <footer className="mt-8">
-        <Footer />
-      </footer>
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Toaster />
-        <RouterProvider router={router}
-            future={{ v7_startTransition: true, v7_relativeSplatPath: true, }}
-        />
     </React.StrictMode>
 );
