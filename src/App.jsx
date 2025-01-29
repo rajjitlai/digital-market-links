d3531f142079472aca00c26377d35f155d6cf05f
RAJJIT <2.0lairaj@gmail.com>
2025-01-30 00:10:42 +0530

    fixes 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./auth/Login";
 import Signup from "./auth/SignUp";
 import Dashboard from "./pages/personal/Dashboard";
+import { useAuth } from "./context/AuthContext";
 
 const PrivateRoute = ({ children }) => {
   const { user } = useAuth();
