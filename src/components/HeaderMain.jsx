43cdd75da1ef570723b7affe595147943774d100
RAJJIT <2.0lairaj@gmail.com>
2025-01-30 16:00:14 +0530

    nav done

--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import logo from "../assets/icon.png"
 import { BsSearch } from 'react-icons/bs'
 import { BiUser } from 'react-icons/bi'
--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
-import { AiOutlineHome } from 'react-icons/ai'
-import { FiHeart } from 'react-icons/fi'
-import { IoMenuOutline } from 'react-icons/io5'
+import { useState } from 'react';
+import { IoMenuOutline } from 'react-icons/io5';
+import { AiOutlineClose } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
+import { navItems } from '../constants';
 
-const MobileNavbar = () => {
+const MobileSidebar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className='lg:hidden fixed bottom-0 w-full bg-white left-[50%] -translate-x-[50%] max-w-[500px] mob_navbar px-8'>
-            <div className='flex justify-between text-[28px] py-2 cursor-pointer'>
-                <IoMenuOutline />
-                <AiOutlineHome />
-                <FiHeart />
+        <div className="lg:hidden fixed top-4 left-4 z-50">
+            <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="text-3xl p-2 bg-white rounded-full shadow-md"
+            >
+                {isOpen ? <AiOutlineClose /> : <IoMenuOutline />}
+            </button>
+
+            <div className={`fixed top-0 left-0 h-full w-64 bg-white shadow-lg transform ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out`}>
+                <button
+                    onClick={() => setIsOpen(false)}
+                    className="absolute top-4 right-4 text-3xl"
+                >
+                    <AiOutlineClose />
+                </button>
+
+                <div className="p-6 mt-12 flex flex-col justify-between h-full">
+                    <div>
+                        {navItems.map((item) => (
+                            <div key={item.id} className="text-xl mb-4">
+                                <Link to={item.path} className='hover:text-primary hover:underline'>{item.label}</Link>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="mt-auto border-t pt-4 pb-10">
+                        <div className="flex items-center gap-2 mb-4">
+                            <FaUserCircle className="text-3xl" />
+                            <span className="text-xl">User Name</span>
+                        </div>
+                        <button className="w-full bg-primary text-white py-2 rounded-lg hover:bg-secondary">Logout</button>
+                    </div>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default MobileNavbar
+export default MobileSidebar;
