fbd48b6afaa0b056b706e8feb9efb7d5abdb98c3
Rajjit Laishram <93727816+rajjitlai@users.noreply.github.com>
2025-01-30 16:07:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./auth/Login";
-import Signup from "./auth/SignUp";
+import Signup from "./auth/Signup";
 import Dashboard from "./pages/personal/Dashboard";
 import { useAuth } from "./context/AuthContext";
 
